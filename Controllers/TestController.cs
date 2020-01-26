ed0b4a0c46183b0a73846042d7615785691ecf71
BELABES Abderrahmane <abelabes@enac.dz>
2020-01-26 13:18:06 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BonDeSortieWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        // GET: Test/Part
+        public ActionResult Part()
+        {
+            return View();
+        }
+
+      
+
+    }
+}
---
+++ Data/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BonDeSortieWeb.Data
+{
+    public class Employee
+    {
+        public  string nomEmp { get; set; }
+        public  string prenomEmp { get; set; }
+        public  DateTime dateNaissanceEmp { get; set; }
+        public DateTime dateInstallation { get; set; }
+    }
+}
